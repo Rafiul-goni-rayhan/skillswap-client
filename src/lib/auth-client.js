2f7072fcf99e3ab0c785f3b3d0de6dbd88b4a138
Rafiul-goni-rayhan <rayhan.cse8.bu@gmail.com>
2026-06-29 13:39:01 +0600

    env thik korchi

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   // baseURL: "https://skillswap-client-virid.vercel.app",
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [jwtClient()],
 });
 
